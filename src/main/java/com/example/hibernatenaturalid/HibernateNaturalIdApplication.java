fec30793eda3ea74d1da775fbb024f771bd52df2
mtumilowicz <michal.tumilowicz01@gmail.com>
2018-10-20 22:53:15 +0200

    Initial commit

---
+++ src/main/java/com/example/hibernatenaturalid/HibernateNaturalIdApplication.java
@@ -0,0 +1,12 @@
+package com.example.hibernatenaturalid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateNaturalIdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HibernateNaturalIdApplication.class, args);
+    }
+}
